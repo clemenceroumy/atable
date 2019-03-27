bdbcd00ce766720b25ed4732c7218959851060b9
Clemence Roumy <clemenceroumy@gmail.com>
2019-03-27 17:14:46 +0100

    Styling a bit the Restaurant details page

---
+++ src/components/btnBack.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-btn icon @click="goBack()" dark>
+      <v-icon>arrow_back_ios</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BtnBack",
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    }
+  }
+};
+</script>
--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -1,13 +1,40 @@
 <template>
-  <div></div>
+  <v-slide-y-transition mode="out-in">
+    <v-container grid-list-xs>
+      <btnBack/>
+
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-img :src="restaurant.logoRestaurant" contain alt="logo of the restaurant"/>
+        </v-flex>
+
+        <v-flex xs12>
+          <h1>{{ restaurant.nomRestaurant }}</h1>
+          <h3>{{ `${$t('restaurant.numberPlace')} : ${restaurant.nombrePlaces}`}}</h3>
+          <p>{{ restaurant.descriptionRestaurant }}</p>
+          <h3>{{ `${$t('restaurant.type')} : ` }}</h3>
+          <ul>
+            <li v-for="type in restaurant.types" :key="type.id">{{type.libelle}}</li>
+          </ul>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-btn color="success">{{ $t('restaurant.book') }}</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-slide-y-transition>
 </template>
 
 <script>
+import btnBack from "../components/btnBack";
+
 import axios from "axios";
 import config from "../config/config.js";
 
 export default {
   name: "Restaurant",
+  components: { btnBack },
   data() {
     return {
       restaurantId: this.$route.params.idRestaurant,
