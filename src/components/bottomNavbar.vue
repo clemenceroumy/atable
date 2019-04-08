9b3ad5f5711e42cd1d42b33b1b38ba734948f1bc
Clemence Roumy <clemenceroumy@gmail.com>
2019-04-08 18:04:15 +0200

    Change color of bottomNavbar's icons

--- src/components/bottomNavbar.vue
+++ src/components/bottomNavbar.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
     <v-bottom-nav fixed value="true">
-      <v-btn to="home" flat :value="$t('navbar.home')">
+      <v-btn to="home" flat color="#f46b45" :value="$t('navbar.home')">
         <span>{{$t('navbar.home')}}</span>
         <v-icon>home</v-icon>
       </v-btn>
 
-      <v-btn to="history" flat :value="$t('navbar.reservation')">
+      <v-btn to="history" flat color="#f46b45" :value="$t('navbar.reservation')">
         <span>{{$t('navbar.reservation')}}</span>
         <v-icon>restaurant_menu</v-icon>
       </v-btn>
 
-      <v-btn to="account" flat :value="$t('navbar.account')">
+      <v-btn to="account" flat color="#f46b45" :value="$t('navbar.account')">
         <span>{{$t('navbar.account')}}</span>
         <v-icon>account_circle</v-icon>
       </v-btn>
